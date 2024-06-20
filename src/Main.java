e83f34d0813217f1164aeabe9182105c58ec9aba
Andres J. Nazzari <andresnazzari@gmail.com>
2024-06-20 01:41:06 -0300

    Fix warnings

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import Infrastructure.DbConn;
-import Infrastructure.Repositories.UserRepository;
-import javax.swing.*;
 import Presentation.Views.LoginForm;
-import Core.Application.Services.PasswordService;
+
 
 import java.sql.SQLException;
 
@@ -11,12 +9,10 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         System.out.print("Hello and welcome!");
-
-        // Este usuario solo se crea si no existe el usuario con dni 33028540
         DbConn.createAdminUser();
 
         LoginForm loginForm = new LoginForm();
         loginForm.setVisible(true);
-        loginForm.setLocationRelativeTo(null); // Centrar la ventana
+        loginForm.setLocationRelativeTo(null);
     }
 }
